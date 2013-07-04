ae972eab903b9f08f4039ea1ba19bd48362fb3ef
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-07-04 17:03:36 +0100

    Use struts for modelling data

--- lib/intermine/jbrowse/model.rb
+++ lib/intermine/jbrowse/model.rb
@@ -3,71 +3,63 @@ module InterMine
 
         module Hashlike
 
-            attr_accessor @@fields
-
             def to_json(*a)
                 self.to_h.to_json(*a)
             end
 
             def to_h
-                Hash[ instance_variables.map {|ivar| [f, send(f)]} ]
+                Hash[ each_pair.to_a ]
             end
 
         end
 
-        class BasicFeature
-
-            @@fields = [:type, :name, :uniqueID, :start, :end, :strand]
+        BasicFeature = Struct.new("BasicFeature", :type, :name, :uniqueID, :start, :end, :strand) do 
 
             include Hashlike
 
-            def initialize(locatedFeature)
+            def self.create(locatedFeature)
                 feature = locatedFeature.feature
-                @type = feature.sequenceOntologyTerm.name
-                @name = feature.name or feature.symbol
-                @uniqueID = feature.primaryIdentifier
-                @start = locatedFeature.start
-                @end = locatedFeature.end
-                @strand = locatedFeature.strand
+                type = feature.sequenceOntologyTerm.name
+                name = feature.name or feature.symbol
+                uniqueID = feature.primaryIdentifier
+                BasicFeature.new(type, name, uniqueID, locatedFeature.start, locatedFeature.end, locatedFeature.strand)
             end
-
         end
 
-        class ReferenceSequence
-
-            @@fields = [:start, :end, :seq]
+        ReferenceSequence = Struct.new("ReferenceSequence", :start, :end, :seq) do
 
             include Hashlike
 
-            def initialize(segment, seq)
-                @seq = seq
-                @start = segment[:start] or 0
-                @end = segment[:end]
+            def self.create(segment, seq)
+                ReferenceSequence.new((segment[:start] || 0), segment[:end], seq)
             end
-
         end
 
-        class Feature < BasicFeature
+        Feature = Struct.new("Feature", :type, :name, :uniqueID, :start, :end, :strand, :subfeatures) do
 
-            attr_accessor :subfeatures
+            include Hashlike
 
-            def initialize(locatedFeature)
-                super(locatedFeature)
+            def self.create(locatedFeature)
+                parsed = BasicFeature.create(locatedFeature).values
+
                 feature = locatedFeature.feature
                 if feature.nil?
                     raise "This location has no feature! #{ locatedFeature }"
                 end
                 child_features = feature.locatedFeatures
                 if child_features.nil?
-                    @subfeatures = []
+                    subfeatures = []
                 else
-                    @subfeatures = child_features.map {|sf| BasicFeature.new(sf)}
+                    subfeatures = child_features.map {|sf| BasicFeature.create(sf)}
                 end
+                values = parsed + [subfeatures]
+                Feature.new(*values)
             end
 
             def to_h
-                super.merge({:subfeatures => @subfeatures.map(&:to_h)})
+                Hash[ each_pair.map{ |k, v| [k, (k == :subfeatures) ? v.map(&:to_h) : v]}]
             end
+
         end
     end
 end
